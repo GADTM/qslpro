92a7a8070c1caa202793a98d150bfe2882638476
GADTM <activacionesgadtm@gmail.com>
2026-05-18 19:50:32 -0300

    Update google-callback.js

--- netlify/functions/google-callback.js
+++ netlify/functions/google-callback.js
@@ -8,46 +8,96 @@ exports.handler = async function(event){
         if(!code){
 
             return{
-
                 statusCode:400,
-
-                body:"No se recibió código OAuth"
+                body:"No se recibió CODE"
             };
         }
+
+        // =========================
+        // DATOS GOOGLE
+        // =========================
+
+        const clientId =
+        process.env.GOOGLE_CLIENT_ID;
+
+        const clientSecret =
+        process.env.GOOGLE_CLIENT_SECRET;
+
+        const redirectUri =
+        'https://qslpro2.netlify.app/.netlify/functions/google-callback';
+
+        // =========================
+        // PEDIR TOKEN
+        // =========================
+
+        const params =
+        new URLSearchParams();
+
+        params.append('code', code);
+
+        params.append('client_id', clientId);
+
+        params.append('client_secret', clientSecret);
+
+        params.append('redirect_uri', redirectUri);
+
+        params.append('grant_type', 'authorization_code');
+
+        const tokenResponse =
+        await fetch(
+            'https://oauth2.googleapis.com/token',
+            {
+                method:'POST',
+                headers:{
+                    'Content-Type':
+                    'application/x-www-form-urlencoded'
+                },
+                body:params
+            }
+        );
+
+        const tokenData =
+        await tokenResponse.json();
+
+        console.log(tokenData);
 
         return{
 
             statusCode:200,
 
             headers:{
-                "Content-Type":"text/html; charset=utf-8"
+                "Content-Type":
+                "text/html; charset=utf-8"
             },
 
             body:`
+
             <html>
+
             <body style="
                 font-family:Arial;
+                background:#f0f0f0;
                 text-align:center;
                 padding-top:50px;
-                background:#f0f0f0;
             ">
 
-            <h2>✅ Gmail conectado correctamente</h2>
+            <h2>
+            ✅ Gmail conectado correctamente
+            </h2>
 
             <p>
-            OAuth funcionando OK
+            TOKEN recibido correctamente
             </p>
 
-            <p>
-            Código recibido:
-            </p>
+            <h3>Access Token:</h3>
 
             <textarea style="
                 width:80%;
-                height:120px;
-            ">${code}</textarea>
+                height:150px;
+            ">${tokenData.access_token || 'NO TOKEN'}</textarea>
 
             </body>
+
             </html>
             `
         };

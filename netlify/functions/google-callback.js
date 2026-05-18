fd5b689ffa93ba759fc015a6e23085280c44b0ec
GADTM <activacionesgadtm@gmail.com>
2026-05-18 19:44:44 -0300

    Update google-callback.js

--- netlify/functions/google-callback.js
+++ netlify/functions/google-callback.js
@@ -20,7 +20,7 @@ exports.handler = async function(event){
             statusCode:200,
 
             headers:{
-                "Content-Type":"text/html"
+                "Content-Type":"text/html; charset=utf-8"
             },
 
             body:`
